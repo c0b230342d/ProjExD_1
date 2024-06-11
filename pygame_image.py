c07e1c147a6f0169f0f3d406b5e7004e8e85f6c1
Kazuki.Oki <c0b230342d@edu.teu.ac.jp>
2024-06-11 16:45:16 +0900

    練習8 方向キーに応じたこうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  #こうかとんのRectを抽出
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -24,8 +26,17 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
-        kk_rct = kk_img.get_rect()  #こうかとんのRectを抽出
-        kk_rct.center = 300, 200
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+
         screen.blit(kk_img, kk_rct)  #kk_rctの設定に従って貼り付け
         pg.display.update()
         tmr += 1
